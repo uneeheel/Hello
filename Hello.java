a8b866930f4b79b03fb589e54e7ae9a9057ec66c
uneeheel <87410866+uneeheel@users.noreply.github.com>
2022-07-04 16:58:57 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+	//main 메서드
+	//시작점이 되려면 반드시 public static void main(String[] args)가 있어야 함
+	public static void main(String[] args) {
+		//주석
+		System.out.println("Hello World");
+		System.out.println("이예은");
+		System.out.println(22);
+		System.out.println('여');
+		System.out.println(true); //false
+		System.out.println(155.4); //실수
+		System.out.println(""+null); //값이 없음을 의미
+	}
+
+}
